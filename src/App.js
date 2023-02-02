bccb9094ed954d0825f9ee37f5a0112f07cb15c1
Yusupov Ilnaz <rilok21work@gmail.com>
2023-02-02 18:59:36 +0200

    react-cg assignment 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpenses from "./components/NewExpense/NewExpence";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -22,9 +23,17 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpenses
+        onAddExpense={addExpenseHandler}
+      />
       <Expenses
         items={expenses}
       />
