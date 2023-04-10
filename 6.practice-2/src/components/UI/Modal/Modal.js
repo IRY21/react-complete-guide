ab91231e0ae5df4cad26c248f2a7ef911db6eef5
Yusupov Ilnaz <rilok21work@gmail.com>
2023-04-10 07:34:52 +0300

    react-cg upd Modal

--- 6.practice-2/src/components/UI/Modal/Modal.js
+++ 6.practice-2/src/components/UI/Modal/Modal.js
@@ -1,18 +1,25 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import classes from './Modal.module.css';
 
-const Modal = ({ children, isOpen, closeModal }) => {
+const ModalEl = ({ children, closeModal }) => {
   return (
     <>
-      {isOpen && (
-        <>
-          <div className={classes.backdrop} onClick={closeModal} />
-          <div className={classes.modal}>
-            {children}
-          </div>
-        </>
-      )}
+      <div className={classes.backdrop} onClick={closeModal} />
+      <div className={classes.modal}>
+        {children}
+      </div>
+    </>
+  );
+}
+
+const Modal = ({ children, isOpen, closeModal }) => {
+  const modalContainer = document.getElementById('overlays');
+
+  return (
+    <>
+      {isOpen && ReactDOM.createPortal(<ModalEl closeModal={closeModal}>{children}</ModalEl>, modalContainer)}
     </>
   );
 }
